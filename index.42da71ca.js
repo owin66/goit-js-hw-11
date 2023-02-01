2b66ef32303e5925b5808653f3338b689901804e
owin66 <78900270+owin66@users.noreply.github.com>
2023-02-01 21:54:19 +0000

    Deploying to gh-pages from @ owin66/goit-js-hw-11@1e8b915c73c2b57c2d4be726692729d7427920ff 🚀

---
+++ index.42da71ca.js
@@ -0,0 +1,2 @@
+!function(){var t=0,e=0;window.addEventListener("scroll",(function(){var n=document.querySelector(".container"),o=n.offsetHeight,i=window.pageYOffset,s=e-i;t=Math.min(0,Math.max(-o,t+s)),n.style.position=i>=o?"fixed":0===i?"absolute":n.style.position,n.style.transform="translateY(".concat("fixed"===n.style.position?t:0,"px)"),e=i}))}();
+//# sourceMappingURL=index.42da71ca.js.map
